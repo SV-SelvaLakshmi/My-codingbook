08b8fd2dbd0650cca8186dc4bc13e65ef68fff80
Selva Lakshmi <53254307+SV-SelvaLakshmi@users.noreply.github.com>
2020-07-30 09:58:28 -0700

    Create Accept Print Hexadecimal Values.c

---
+++ Accept Print Hexadecimal Values.c
@@ -0,0 +1,9 @@
+//Write a program to accept the hexadecimal value and print it in the hexadecimal.
+#include<stdio.h>
+int main(){
+    int Hex_Value;
+    scanf("%X",&Hex_Value); //%X is used and it print A to F
+    printf("%X",Hex_Value);//%x is also used and it print a to f
+  
+    return 0;
+}

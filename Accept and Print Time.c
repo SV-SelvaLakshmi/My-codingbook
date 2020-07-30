5f19ad8c71607f4bc2dfbde29464b5ee14f3fee4
Selva Lakshmi <53254307+SV-SelvaLakshmi@users.noreply.github.com>
2020-07-30 10:01:51 -0700

    Create Accept and Print Time.c

---
+++ Accept and Print Time.c
@@ -0,0 +1,8 @@
+//Write a program to accept the time and print it.
+#include<stdio.h>
+int main(){
+  char times[20];
+  scanf("%s",&times);//This will take  23:55:09 as input
+  printf("%s",times);//This will print 23:55:09
+  return 0;
+}

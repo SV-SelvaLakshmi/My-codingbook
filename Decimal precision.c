90d2ac645347c8b4e8a11ced71d9fa553bc35727
Selva Lakshmi <53254307+SV-SelvaLakshmi@users.noreply.github.com>
2020-07-30 09:52:46 -0700

    Create Decimal precision.c

---
+++ Decimal precision.c
@@ -0,0 +1,10 @@
+//Write a program to accept an input real value with decimals and print it as rounded off to 2 decimals
+#include<stdio.h>
+#include<math.h>
+int main(){
+   double user;
+   scanf("%lf",&user);
+   printf("%.2lf",user);
+
+    return 0;
+}

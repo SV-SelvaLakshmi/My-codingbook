ee2081c404e515d69998fdc97b486768051f86e7
Selva Lakshmi <53254307+SV-SelvaLakshmi@users.noreply.github.com>
2020-07-30 10:05:17 -0700

    Create format printing.c

---
+++ format printing.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+   long long int number1,number2;
+   scanf("%Ld %lld",&number1,&number2);
+   printf("Number1 = %lld\n",number1);
+   printf("Number2 = %lld",number2);
+   return 0;
+}
